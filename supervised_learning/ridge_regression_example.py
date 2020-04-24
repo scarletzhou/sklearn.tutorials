a147eaf771923a465a1803c04fc7d92209c0b38a
zhouhan <15053751006@163.com>
2020-04-24 09:33:20 +0800

    linear regression example

---
+++ supervised_learning/ridge_regression_example.py
@@ -0,0 +1,37 @@
+# Author: Fabian Pedregosa -- <fabian.pedregosa@inria.fr>
+# License: BSD 3 clause
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+# X 是 10x10 希尔伯特矩阵
+X = 1. / (np.arange(1, 11) + np.arange(0, 10)[:, np.newaxis])
+y = np.ones(10)
+
+# #############################################################################
+# 计算路径
+
+n_alphas = 200
+alphas = np.logspace(-10, -2, n_alphas)
+
+coefs = []
+for a in alphas:
+    ridge = linear_model.Ridge(alpha=a, fit_intercept=False)
+    ridge.fit(X, y)
+    coefs.append(ridge.coef_)
+
+# #############################################################################
+# 显示结果
+
+ax = plt.gca()
+
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
+plt.xlabel('alpha')
+plt.ylabel('weights')
+plt.title('Ridge coefficients as a function of the regularization')
+plt.axis('tight')
+plt.show()
